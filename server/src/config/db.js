0eb92d4ad08db43890e1d4ab5ffea20573b3d18c
kev-69 <bismarkobuobi19@gmail.com>
2025-04-21 16:12:22 +0000

    Add db.js file for database configuration and connection testing

---
+++ server/src/config/db.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        logging: false, // Disable logging for cleaner output
+    }
+)
+
+// Test the connection to the database
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports = sequelize;
