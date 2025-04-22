21230c5b1cbbd9d460ae905c27c5845d0e96a995
kev-69 <bismarkobuobi19@gmail.com>
2025-04-22 06:57:02 +0000

    Add Order model for managing order details and statuses

---
+++ server/src/models/order-model.js
@@ -0,0 +1,47 @@
+const sequeliae = require('../config/db');
+const { DataTypes } = require('sequelize');
+
+const Order = sequeliae.define('Order', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    totalAmount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+    },
+    orderStatus: {
+        type: DataTypes.ENUM('processing', 'shipped', 'completed', 'cancelled'),
+        defaultValue: 'pending',
+    },
+    paymentStatus: {
+        type: DataTypes.ENUM('pending', 'paid', 'failed'),
+        defaultValue: 'pending',
+    },
+    paymentMethod: {
+        type: DataTypes.ENUM('CreditCard', 'MobileMoney'),
+        defaultValue: 'MobileMoney',
+    }
+},  {
+    timestamps: true,
+    tableName: 'orders',
+})
+
+// Order.associate = (models) => {
+//     Order.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+//     Order.hasMany(models.OrderItem, { foreignKey: 'orderId', as: 'items' });
+//     Order.hasMany(models.ShippingAddress, { foreignKey: 'orderId', as: 'shippingAddress' });
+//     Order.hasMany(models.BillingAddress, { foreignKey: 'orderId', as: 'billingAddress' });
+// }
+
+module.exports = Order;
