61eadc1b62e75b79dbb256ef3bd197172d739539
kev-69 <bismarkobuobi19@gmail.com>
2025-04-22 14:40:59 +0000

    Refactor index.js to organize middleware and route imports

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 const exress = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const errorHandler = require('./middlewares/error-handler');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = require('./config/db');
 
 // call routes here
-
+const authRoutes = require('./routes/auth-routes/auth-routes');
 
 const app = exress();
 const PORT = process.env.PORT || 5000;
@@ -22,9 +24,13 @@ sequelize.sync({ force: false })
 app.use(cors());
 app.use(exress.json());
 app.use(exress.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 // import routes here
+app.use('/api/auth', authRoutes);
 
+// error handler middleware
+app.use(errorHandler)
 
 // start server
 app.listen(PORT, () => {
