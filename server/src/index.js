c0ccd3cfaccface7447e91eb5aa24cee2735d67e
kev-69 <bismarkobuobi19@gmail.com>
2025-04-22 06:53:23 +0000

    Add database synchronization logic to server startup

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const sequelize = require('./config/db');
+
 // call routes here
 
 
@@ -10,12 +12,19 @@ const app = exress();
 const PORT = process.env.PORT || 5000;
 
 // sync db here
+sequelize.sync({ force: false })
+.then(() => {
+  console.log('Database synced successfully');
+}).catch((error) => {
+  console.error('Error syncing database:', error);
+});
 
 app.use(cors());
 app.use(exress.json());
 app.use(exress.urlencoded({ extended: true }));
 
 // import routes here
+
 
 // start server
 app.listen(PORT, () => {
